9a31e56342095ac92b3effcf4d20bfe34df84e3f
Jared Fernandez <jaredfern@cmu.edu>
2021-07-26 22:39:01 -0400

    init experimental commit

--- main.py
+++ main.py
@@ -1,55 +1,118 @@
+import argparse
 import logging
 import time
 
+import numpy as np
 import torch
 import transformers
 import yaml
 
-import numpy as np
-
-# TODO: Add pretrained weights initialization for all architectures
 NAME2MODEL = {
-    "bert": transformers.BertModel,
-    "roberta": transformers.RobertaModel,
-    "deberta": transformers.DebertaModel,
-    "distilbert": transformers.DistilBertModel,
-    "funnel_transformer": transformers.FunnelModel,
-    "ibert": transformers.IBertModel,
-    "longformer": transformers.LongformerModel,
-    "mobile_bert": transformers.MobileBertModel,
-    "reformer": transformers.ReformerModel,
-    "squeeze_bert": transformers.SqueezeBertModel,
+    "bert": [transformers.BertModel, transformers.BertTokenizer, "bert-base-cased"],
+    "roberta": [
+        transformers.RobertaModel,
+        transformers.RobertaTokenizer,
+        "roberta-base",
+    ],
+    "deberta": [
+        transformers.DebertaModel,
+        transformers.DebertaTokenizer,
+        "microsoft/deberta-base",
+    ],
+    "distilbert": [
+        transformers.DistilBertModel,
+        transformers.DistilBertTokenizer,
+        "distilbert-base-cased",
+    ],
+    "funnel_transformer": [
+        transformers.FunnelModel,
+        transformers.FunnelTokenizer,
+        "funnel-transformer/small",
+    ],
+    "ibert": [
+        transformers.IBertModel,
+        transformers.BertTokenizer,
+        "kssteven/ibert-roberta-based",
+    ],
+    "longformer": [
+        transformers.LongformerModel,
+        transformers.LongformerTokenizer,
+        "allenai/longformer-base-4096",
+    ],
+    "mobile_bert": [
+        transformers.MobileBertModel,
+        transformers.MobileBertTokenizer,
+        "google/mobilebert-case",
+    ],
+    "reformer": [
+        transformers.ReformerModel,
+        transformers.ReformerTokenizer,
+        "reformer-enwik8",
+    ],
+    "squeeze_bert": [
+        transformers.SqueezeBertModel,
+        transformers.SqueezeBertTokenizer,
+        "squeezebert/squeezebert-uncased",
+    ],
 }
 
 
-def main(model_name="bert", random_text=False, requires_grad=True, batch_size=1, iters=20,
-         use_cuda=False):
+def main(opts, model_name):
+    # Setup Logger
+    file_handler = logging.FileHandler(filename=f"log/{model_name}.log")
+    file_handler.setLevel(logging.DEBUG)
+    logger = logging.getLogger(model_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
     init_time = time.time()
-    tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-cased")
-    model = NAME2MODEL[model_name].from_pretrained("bert-base-cased")
-    model.requires_grad_(requires_grad)
+    model_fn, tokenizer, checkpoint = NAME2MODEL[model_name]
+    logger.info(f"Loading {model_name} model from {checkpoint}")
+    tokenizer = tokenizer.from_pretrained(checkpoint)
+    model = model_fn.from_pretrained(checkpoint)
+    model.requires_grad_(opts["requires_grad"])
     model.eval()
 
-    seq_lengths = [2 ** seqlen for seqlen in range(10)]
+    seq_lengths = [
+        2 ** seqlen for seqlen in range(10)
+    ]  # Eval on sequence lengths up from 1 to 512
+    wallclock_times = []
     for seq_len in seq_lengths:
         time_per_example = []
-        for _ in range(iters):
-            if random_text:
+        for _ in range(opts["iters"]):
+
+            if opts["randomized_text"]:
                 input_ids = torch.randint(tokenizer.vocab_size, (seq_len))
             else:
                 tokens = seq_len * ["a"]
                 input_ids = torch.tensor(tokenizer.convert_tokens_to_ids(tokens))
             input_ids = input_ids.unsqueeze(0)
-            if use_cuda:
+            if opts["use_cuda"]:
                 input_ids = input_ids.to("cuda")
                 model.to("cuda")
 
             init_example_time = time.time()
-            outputs = model(input_ids)
+            _ = model(input_ids)
             time_per_example.append(time.time() - init_example_time)
-        print(f"Model: {model_name}, Seqlen: {seq_len} ---- Time per example:"
-                    f" {np.mean(time_per_example)}")
+        average_time = np.mean(time_per_example)
+        wallclock_times.append(average_time)
+        logger.info(f"Sequence Length: {seq_len}, Time per example: {average_time}")
+    logger.info(f"Averaged Times: {wallclock_times}")
+    logger.info(f"Total Runtime: {time.time() - init_time}")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str)
+    parser.add_argument("--model", type=str)
+    opts = parser.parse_args()
+
+    # Load config
+    with open(opts.config, "r") as config_file:
+        params = yaml.safe_load(config_file)
+
+    if opts.model == "all":
+        for model in NAME2MODEL.keys():
+            main(params, model)
+    else:
+        main(params, opts.model)
