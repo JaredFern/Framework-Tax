65421e20d0859b6d8513d4c99d6c10b752be0670
Jared Fernandez <jaredfern@cmu.edu>
2022-10-27 19:55:39 -0400

    Update and rename run_pretrained.sh to run_pretrained_fp32.sh

---
+++ pytorch/run_pretrained_fp32.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+PLATFORM=a100
+DEVICE=cuda
+
+declare -a VISION_MODELS=("vit32" "resnet50")
+for MODEL in ${VISION_MODELS[@]}; do
+    python3 main_pretrained.py \
+        --model $MODEL --model_config config/models/vision.yaml\
+        --platform $PLATFORM --device $DEVICE --device_config config/devices/${DEVICE}.yaml \
+        --results_dir experiments/pretrained --exp_name $EXP_NAME;
+done;   
+
+declare -a LANG_MODELS=("bert")
+for PRECISION_FLAG in  ${PRECISIONS[@]}; do 
+    for MODEL in ${LANG_MODELS[@]}; do
+      python3 main_pretrained.py \
+        --model $MODEL --model_config config/models/transformers.yaml\
+        --platform $PLATFORM --device $DEVICE --device_config config/devices/${DEVICE}.yaml \
+        --results_dir experiments/pretrained --exp_name $EXP_NAME;
+done; 
